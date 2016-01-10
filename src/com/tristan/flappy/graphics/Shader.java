284b4b6e4fe3c6d8c0f76ee0d60a80f4a2715651
tristangaebler <Tristan.Gaebler@gmail.com>
2016-01-09 21:05:41 -0700

    Stopped at 1:00:48

--- src/com/tristan/flappy/graphics/Shader.java
+++ src/com/tristan/flappy/graphics/Shader.java
@@ -1,7 +1,7 @@
 package com.tristan.flappy.graphics;
 
-import com.tristan.flappy.utils.ShaderUtils;
 import static org.lwjgl.opengl.GL20.*;
+import com.tristan.flappy.utils.ShaderUtils;
 
 public class Shader {
 
@@ -9,6 +9,14 @@ public class Shader {
 	
 	public Shader(String vertex, String fragment) {
 		ID = ShaderUtils.load(vertex, fragment);
+	}
+	
+	public int getUniform(String name) {
+		return glGetUniform(ID, name);
+	}
+	
+	public void setUniform1i(String name, int value) {
+		glUniformi(location, v0);
 	}
 	
 	public void enable() {
