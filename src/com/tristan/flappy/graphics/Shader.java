e1d79293bda3c44cf85eed37d0187688c44608c1
tristangaebler <Tristan.Gaebler@gmail.com>
2016-01-10 19:12:56 -0700

    Stopped  around 1:2

--- src/com/tristan/flappy/graphics/Shader.java
+++ src/com/tristan/flappy/graphics/Shader.java
@@ -1,22 +1,37 @@
 package com.tristan.flappy.graphics;
 
 import static org.lwjgl.opengl.GL20.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import com.tristan.flappy.utils.ShaderUtils;
 
 public class Shader {
 
 	private final int ID;
+	private Map<String, Integer> locationCache = new HashMap<String, Integer>();
 	
 	public Shader(String vertex, String fragment) {
 		ID = ShaderUtils.load(vertex, fragment);
 	}
 	
 	public int getUniform(String name) {
-		return glGetUniform(ID, name);
+		if(locationCache.containsKey(name)) {
+			return locationCache.get(name);
+		}
+		
+		int result = glGetUniformLocation(ID, name);
+		
+		if(result == -1) 
+			System.err.println("Could not find uniform variable " + name + " !");
+		else
+			locationCache.put(name, result);
+		return result;
 	}
 	
 	public void setUniform1i(String name, int value) {
-		glUniformi(location, v0);
+		glUniform1i(getUniform(name), value);
 	}
 	
 	public void enable() {
